6efea1b451cd415ae0ecf28aa37772128ab663b6
blagowhatnow <58713481+blagowhatnow@users.noreply.github.com>
2025-03-10 14:12:13 +0530

    Update vit_model.py

--- vit_model.py
+++ vit_model.py
@@ -2,8 +2,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.optim import Adam
-from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
 from torchvision.transforms import ToTensor, Compose, Normalize, Resize
 from tqdm import tqdm
 
